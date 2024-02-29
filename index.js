608670404abee1b2c0183581c4a5b7698dc6bf4b
zmcclendon1 <122585173+zmcclendon1@users.noreply.github.com>
2024-02-29 11:34:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function toggleForgotPassword() {
 //Defines
     let securityQuestionOne = document.getElementById("secquest1");
     let securityQuestionTwo = document.getElementById("secquest2");
+//Forgot Password Functionality
     securityQuestionOne.classList.toggle("hidden");
     securityQuestionTwo.classList.toggle("hidden");
 }
